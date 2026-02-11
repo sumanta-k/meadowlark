39529e4515b80f1a99e578889219a769ad0cfdcb
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-02-11 22:09:48 +0530

    modify the path to handlebars location

--- meadowlark.js
+++ meadowlark.js
@@ -12,32 +12,26 @@ app.set("view engine", "handlebars");
 app.set("port", process.env.PORT || 3000);
 
 app.get("/", function (req, res) {
-    res.type("text/plain");
-    res.status(200);
-    res.send("Meadowlark Travel");
+    res.render("home");
 });
 
 app.get("/about", function (req, res) {
-    res.type("text/plain");
-    res.status(200);
-    res.send("About Meadowlark Travel");
+    res.render("about");
 });
 
 // custom 404 page
 
-app.use(function (req, res) {
-    res.type("text/plain");
+app.use(function (req, res, next) {
     res.status(404);
-    res.send("404 - Page Not Found");
+    res.render("404");
 });
 
 // custom 500 page
 
 app.use(function (err, req, res, next) {
     console.error(err.stack);
-    res.type("text/plain");
     res.status(500);
-    res.send("500 - Internal Sever Error");
+    res.render(500);
 });
 
 app.listen(app.get("port"), function () {
